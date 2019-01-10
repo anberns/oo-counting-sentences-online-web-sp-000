4d73d8df1f0a8df64dd061678ea0831d0895f2dd
Aaron Berns <anberns@gmail.com>
2019-01-10 14:34:37 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -17,6 +17,7 @@ class String
   def count_sentences
     count = 0 
     self.each do |str|
+      puts str
       if str.sentence? || str.question? || str.exclamation?
         count += 1
       end
