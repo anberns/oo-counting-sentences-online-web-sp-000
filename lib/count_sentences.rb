0cc2940c5199fe40a6a62861be79077e9dc5a203
Aaron Berns <anberns@gmail.com>
2019-01-10 14:32:44 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -15,6 +15,12 @@ class String
   end
 
   def count_sentences
-
+    count = 0 
+    self.each do |str|
+      if str.sentence? || str.question? || str.exclamation?
+        count += 1
+      end
+    end
+    count
   end
 end
